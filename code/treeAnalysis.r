d6053889535bfc774ceb904b028ce5b33b868041
Hurlbert <hurlbert@bio.unc.edu>
2019-07-25 09:38:23 -0400

    v1 of betweenModelPCAPlot working

--- code/treeAnalysis.r
+++ code/treeAnalysis.r
@@ -26,9 +26,9 @@ treeMetricsPCA = function(treeOutput, vars = 'all') {
   }
   outputSubsetNoNAs = na.omit(treeOutput[!names(treeOutput) == "VPD"])  
   pc = princomp(outputSubsetNoNAs[, names(outputSubsetNoNAs) %in% vars], cor = TRUE)
-  pcaOutput = cbind(outputSubsetNoNAs[, c("model", "simID")], pc$scores) 
+  pcaScores = cbind(outputSubsetNoNAs[, c("model", "simID")], pc$scores) 
   
-  return(list(pcaScores = pcaOutput, pcaLoadings = pc$loadings))
+  return(list(pcaScores = pcaScores, pcaLoadings = pc$loadings))
 }
 
 
@@ -38,45 +38,58 @@ classifyWithinModel = function(treeOutput, withinModelParametersTable) {
 }
 
 
-betweenModelPCAPlot = function(pcaScores,          # dataframe with model, simID, and PC scores
+betweenModelPCAPlot = function(pcaOutput,          # dataframe with model, simID, and PC scores
                                xscore = 1,        # PC score plotted on the x-axis
                                yscore = 2,        # PC score plotted on the y-axis
                                colorBy = 'model', # any variable/parameter name by which to color points
                                pchBy = 16         # categorical variable/parameter name by which to specify point shape 
                                ) {
+  require(gsheet)
+  url = "https://docs.google.com/spreadsheets/d/1pcUuINauW11cE5OpHVQf_ZuzHzhm2VJkCn7-lSEJXYI/edit#gid=2047946073"
+  
+  modelClassification = gsheet2tbl(url)
+  
+  pcaScores = pcaOutput$pcaScores
+  pcaLoadings = pcaOutput$pcaLoadings
   
   colors = c('turquoise', 'orangered', 'yellow2', 'darkblue', 'limegreen', 'magenta', 'blue', 'black')
   pch = c(15, 16, 17, 18, 1, 7, 8, 10)
 
-  colorCode = data.frame(val = unique(pcaScores[, colorBy]), 
-                         color = colors[1:length(unique(pcaScores[, colorBy]))])
-  pchCode = data.frame(val = unique(pcaScores[, pchBy]),
-                       pch = pch[1:length(unique(pcaScores[, pchBy]))])
-                                                
-  plotOutput = left_join(pcaScores, modelClassification, by = "model") %>%
-    left_join(colorCode, by = c((!!colorBy) = 'val')) %>%
-    left_join(pchCode, by = c((!!pchBy) = 'val'))
+  colorCode = data.frame(val = unique(modelClassification[, colorBy]), 
+                         color = colors[1:nrow(unique(modelClassification[, colorBy]))])
+  colorCode$color = as.character(colorCode$color)
+  names(colorCode)[1] = colorBy
+  
+  pchCode = data.frame(val = unique(modelClassification[, pchBy]),
+                       pch = pch[1:nrow(unique(modelClassification[, pchBy]))])
+  names(pchCode)[1] = pchBy
+  
+  plotOutput = left_join(pcaScores, modelClassification, by = c("model", "simID")) %>%
+    left_join(colorCode, by = unname(colorBy)) %>%
+    left_join(pchCode, by = unname(pchBy))
 
-  
-  
-  plot(pc$pca[,paste("Comp.", xscore, sep = "")], pc$pca[, paste("Comp.", yscore, sep = "")], 
-       col = colorBy, cex = 2, 
-       pch = pchBy, xlab = paste("PC", xscore), ylab = paste("PC", yscore), 
-       ylim = c(-max(abs(range(pc$pca[,paste("Comp.", yscore, sep = "")]))), 
-                max(abs(range(pc$pca[,paste("Comp.", yscore, sep = "")])))),
-       xlim = c(-max(abs(range(pc$pca[,paste("Comp.", xscore, sep = "")]))), 
-                max(abs(range(pc$pca[,paste("Comp.", xscore, sep = "")])))))
+  plot(plotOutput[,paste("Comp.", xscore, sep = "")], plotOutput[, paste("Comp.", yscore, sep = "")], 
+       col = plotOutput$color, cex = 2, 
+       pch = plotOutput$pch, xlab = paste("PC", xscore), ylab = paste("PC", yscore), 
+       ylim = c(-max(abs(range(plotOutput[,paste("Comp.", yscore, sep = "")]))), 
+                max(abs(range(plotOutput[,paste("Comp.", yscore, sep = "")])))),
+       xlim = c(-max(abs(range(plotOutput[,paste("Comp.", xscore, sep = "")]))), 
+                max(abs(range(plotOutput[,paste("Comp.", xscore, sep = "")])))))
   legend("topleft", 
-         legend = crossModelSimTable$model,
-         col = crossModelSimTable$color, pch = pchBy)
+         legend = c(toupper(colorBy), colorCode[,1]), bty = "n",
+         col = c('white', colorCode[,2]), pch = 16)
+  
+  legend("topright", 
+         legend = c(toupper(pchBy), pchCode[,1]), bty = "n",
+         col = c('white', rep('black', nrow(pchCode))), pch = c(16, pchCode[,2]))
   
   par(new = TRUE)
   plot(0, 0, type = "n", xaxt = "n", yaxt = "n", xlab = "", ylab = "",
        xlim = c(-1, 1), ylim = c(-1, 1))
   
   # Only print the top variable loadings since it otherwise becomes messy
-  mainLoadings = pc$loadings[order(pc$loadings[, xscore], decreasing = TRUE) <= 4 | 
-                               order(pc$loadings[,yscore], decreasing = TRUE) <= 4, 
+  mainLoadings = pcaLoadings[order(pcaLoadings[, xscore], decreasing = TRUE) <= 4 | 
+                               order(pcaLoadings[,yscore], decreasing = TRUE) <= 4, 
                              c(xscore, yscore)]
   text(mainLoadings[, 1], mainLoadings[, 2], row.names(mainLoadings))  
 }
@@ -89,7 +102,7 @@ varsForPCA = c("PD", "gamma", "beta", "Colless", "Sackin", "Yule.PDA.ratio", "MR
                "VRD", "PSV", "mean.Iprime", "MPD", "MGL_principal_eigenvalue",
                "MGL_asymmetry", "MGL_peakedness", "MGL_eigengap", "nLTT_stat")
 
-pca = treeMetricsPCA(treeOutput, vars = varsForPCA)
+pcaOutput = treeMetricsPCA(treeOutput, vars = varsForPCA)
 
 par(mfrow = c(1,1))
 pcaPlot(pca, xscore = 1, yscore = 2)
