0942a7db9a4fd421471b6f84ba57c06719d361ad
Allen Hurlbert <hurlbert@bio.unc.edu>
2021-07-08 11:32:47 -0400

    adding alignment fcn, moving corrCalcUSE() from  USE_analysis.r

--- code/experiment_analysis_functions.r
+++ code/experiment_analysis_functions.r
@@ -122,6 +122,121 @@ corrCalc = function(experiment, experimentData, mod) {
 }
 
 
+# Function for calculating CI for spearman's rho
+# --from https://stats.stackexchange.com/questions/18887/how-to-calculate-a-confidence-interval-for-spearmans-rank-correlation
+spearman_CI <- function(x, y, alpha = 0.05){
+  rs <- cor(x, y, method = "spearman", use = "complete.obs")
+  n <- sum(complete.cases(x, y))
+  sort(tanh(atanh(rs) + c(-1,1)*sqrt((1+rs^2/2)/(n-3))*qnorm(p = alpha/2)))
+}
+
+
+
+
+
+# For each process (env, nic, dis, mut, tim), examine the relationship between
+# the strength of the parameter value associated with that process and tree metrics.
+
+# 
+
+# Calculate correlations between tree metrics and treatment levels for each model
+corrCalcUSE = function(experiment, experimentData, modelAbbrev, cor.method = 'spearman') {
+  
+  if (! experiment %in% c('env', 'nic', 'dis', 'mut', 'tim', 'com')) {
+    stop("'experiment' but be either 'env', 'nic', 'dis', 'mut', 'com', or 'tim'.")
+  }
+  
+  url <- 'https://docs.google.com/spreadsheets/d/1pcUuINauW11cE5OpHVQf_ZuzHzhm2VJkCn7-lSEJXYI/edit#gid=1171496897'
+  paramKey <- gsheet2tbl(url)
+  
+  # split out the root model abbreviation if necessary
+  mod = word(modelAbbrev, 1, sep = fixed("."))
+  
+  modelParams = read.csv(paste0('trees/uniform_sampling_experiment/', mod, '_USE_parameters.csv'), header = T)
+  
+  
+  # In some cases a single simulation model can be run under different "scenarios" that should
+  # effectively be analyzed separately. In this case, a "scenario" column should be added to
+  # the [model]_USE_parameters.csv file with a code for distinguishing them.
+  
+  if ('scenario' %in% names(modelParams)) {
+    modelParams$model2 = paste(modelParams$model, modelParams$scenario, sep = '.')  
+  } else {
+    modelParams$model2 = modelParams$model
+  }
+  
+  modelData = experimentData %>%
+    right_join(modelParams, by = c('simID', 'model')) %>%
+    filter(model2 == modelAbbrev)
+  
+  experimentalParam = paramKey$parameterName[paramKey$model == modelAbbrev & paramKey$experiment == experiment]
+  
+  if (length(experimentalParam) > 0) { #check that there is a parameter for this experiment
+    
+    metrics = c('log10S', 'PD', 'Gamma', 'Beta', 'Colless', 'Sackin', 'Yule.PDA.ratio', 'MRD', 'VRD', 'PSV',
+                'mean.Iprime', 'MPD', 'VPD', 'nLTT_stat')
+    
+    corDF = data.frame(model = rep(modelAbbrev, length(metrics)*length( experimentalParam)),
+                       experiment = rep(experiment, length(metrics)*length( experimentalParam)),
+                       parameter = rep(experimentalParam, each = length(metrics)),
+                       metric = rep(metrics, length( experimentalParam)),
+                       r = rep(NA, length(metrics)*length( experimentalParam)),
+                       r.L95 = rep(NA, length(metrics)*length( experimentalParam)),
+                       r.U95 = rep(NA, length(metrics)*length( experimentalParam)))
+    
+    for (p in 1:length(experimentalParam)) {   # if multiple params are listed, then cycle through
+      
+      # Some parameters may be configured such that there is a positive correlation between the
+      # strength of the process and the parameter value and vice versa. Multiply correlations through
+      # by this 'sign' so that the interpretation is similar for all parameters:
+      
+      # A positive correlation means that the process increases in strength
+      sign = paramKey$sign[paramKey$model == modelAbbrev & paramKey$parameterName == experimentalParam[p]]
+      
+      
+      for (m in 1:length(metrics)) { # conduct a correlation with the model parameter and each tree metric
+        
+        if (sum(!is.na(modelData[, metrics[m]])) > 3) { # require at least 4 data points to calculate CI's
+          
+          corr = cor.test(modelData[, metrics[m]], modelData[, experimentalParam[p]], 
+                          use = 'na.or.complete', method = cor.method)
+          
+          corDF$r[m + (p-1)*length(metrics)] = sign*corr$estimate
+          
+          if (cor.method == 'pearson') {
+            
+            corDF$r.L95[m + (p-1)*length(metrics)] = sign*corr$conf.int[1]
+            corDF$r.U95[m + (p-1)*length(metrics)] = sign*corr$conf.int[2]
+            
+          } else if (cor.method == 'spearman') {
+            
+            corCI = spearman_CI(modelData[, metrics[m]], modelData[, experimentalParam[p]])
+            corDF$r.L95[m + (p-1)*length(metrics)] = sign*corCI[1]
+            corDF$r.U95[m + (p-1)*length(metrics)] = sign*corCI[2]
+            
+          }
+          
+        } else {
+          
+          corDF$r[m + (p-1)*length(metrics)] = NA
+          corDF$r.L95[m + (p-1)*length(metrics)] = NA
+          corDF$r.U95[m + (p-1)*length(metrics)] = NA
+          
+        }
+        
+      } # end metric loop
+      
+    } # end parameter loop
+    
+  } else { # if there is no parameter associated with this experiment, then NA
+    
+    corDF = NA
+    
+  }
+  
+  return(corDF)  
+}
+
 
 
 # Function for extracting salient features of model output from an lmer model
@@ -141,4 +256,88 @@ extractLMEoutput = function(lmeObject, expName, metricName) {
   
   return(out.df)
   
+}
+
+
+# Function for aligning simulation parameters across models according to the process each parameter is associated with.
+# I.e., create columns for each process (in some cases, two columns for a process because some models have two parameters
+# associated with that process) in which the relevant parameter value is stored.
+
+# Associations between parameters and processes is given here: 
+# https://docs.google.com/spreadsheets/d/1pcUuINauW11cE5OpHVQf_ZuzHzhm2VJkCn7-lSEJXYI/edit#gid=1171496897
+
+# Parameters are multiplied by the sign specified in the above spreadsheet to ensure that the strength of the process
+# increases with an increase in the parameter value.
+
+alignParametersWithProcesses = function(modelAbbrev) {
+  
+  params = read.csv(paste("trees/uniform_sampling_experiment/", modelAbbrev, "_USE_parameters.csv", sep = ""), header = T)
+  
+  params$model2 = ifelse("scenario" %in% names(params), paste(modelAbbrev, ".", params$scenario, sep = ""), modelAbbrev)
+  
+  url <- 'https://docs.google.com/spreadsheets/d/1pcUuINauW11cE5OpHVQf_ZuzHzhm2VJkCn7-lSEJXYI/edit#gid=1171496897'
+  
+  # Here we drop the scenario description, assuming that the process-parameter association is not scenario-dependent
+  paramKey <- gsheet2tbl(url) %>% 
+    mutate(model = word(model, sep = "\\.")) %>%
+    filter(model == modelAbbrev) %>%
+    distinct()
+  
+  # parameter names associated with each process
+  env1name = ifelse("env" %in% paramKey$experiment, paramKey$parameterName[paramKey$experiment == "env"][1], NA)
+  env2name = ifelse(length(unique(paramKey$parameterName[paramKey$experiment == "env"])) == 2, 
+                    paramKey$parameterName[paramKey$experiment == "env"][2], NA)
+  dis1name = ifelse("dis" %in% paramKey$experiment, paramKey$parameterName[paramKey$experiment == "dis"][1], NA)
+  dis2name = ifelse(length(unique(paramKey$parameterName[paramKey$experiment == "dis"])) == 2, 
+                    paramKey$parameterName[paramKey$experiment == "dis"][2], NA)
+  nic1name = ifelse("nic" %in% paramKey$experiment, paramKey$parameterName[paramKey$experiment == "nic"][1], NA)
+  nic2name = ifelse(length(unique(paramKey$parameterName[paramKey$experiment == "nic"])) == 2, 
+                    paramKey$parameterName[paramKey$experiment == "nic"][2], NA)
+  mut1name = ifelse("mut" %in% paramKey$experiment, paramKey$parameterName[paramKey$experiment == "mut"][1], NA)
+  mut2name = ifelse(length(unique(paramKey$parameterName[paramKey$experiment == "mut"])) == 2, 
+                    paramKey$parameterName[paramKey$experiment == "mut"][2], NA)
+  com1name = ifelse("com" %in% paramKey$experiment, paramKey$parameterName[paramKey$experiment == "com"][1], NA)
+  com2name = ifelse(length(unique(paramKey$parameterName[paramKey$experiment == "com"])) == 2, 
+                    paramKey$parameterName[paramKey$experiment == "com"][2], NA)
+  
+  
+  outputDF = params %>%
+    mutate(
+      env1Name = env1name,
+      env2Name = env2name,
+      dis1Name = dis1name,
+      dis2Name = dis2name,
+      nic1Name = nic1name,
+      nic2Name = nic2name,
+      mut1Name = mut1name,
+      mut2Name = mut2name,
+      com1Name = com1name,
+      com2Name = com2name
+    )
+    
+  outputDF$env1 = ifelse(!is.na(outputDF$env1Name), 
+                         outputDF[, env1name] * paramKey$sign[paramKey$parameterName == env1name], NA)
+  outputDF$env2 = ifelse(!is.na(outputDF$env2Name), 
+                         outputDF[, env2name] * paramKey$sign[paramKey$parameterName == env2name], NA)
+  outputDF$dis1 = ifelse(!is.na(outputDF$dis1Name), 
+                         outputDF[, dis1name] * paramKey$sign[paramKey$parameterName == dis1name], NA)
+  outputDF$dis2 = ifelse(!is.na(outputDF$dis2Name), 
+                         outputDF[, dis2name] * paramKey$sign[paramKey$parameterName == dis2name], NA)
+  outputDF$nic1 = ifelse(!is.na(outputDF$nic1Name), 
+                         outputDF[, nic1name] * paramKey$sign[paramKey$parameterName == nic1name], NA)
+  outputDF$nic2 = ifelse(!is.na(outputDF$nic2Name), 
+                         outputDF[, nic2name] * paramKey$sign[paramKey$parameterName == nic2name], NA)
+  outputDF$mut1 = ifelse(!is.na(outputDF$mut1Name), 
+                         outputDF[, mut1name] * paramKey$sign[paramKey$parameterName == mut1name], NA)
+  outputDF$mut2 = ifelse(!is.na(outputDF$mut2Name), 
+                         outputDF[, mut2name] * paramKey$sign[paramKey$parameterName == mut2name], NA)
+  outputDF$com1 = ifelse(!is.na(outputDF$com1Name), 
+                         outputDF[, com1name] * paramKey$sign[paramKey$parameterName == com1name], NA)
+  outputDF$com2 = ifelse(!is.na(outputDF$com2Name), 
+                         outputDF[, com2name] * paramKey$sign[paramKey$parameterName == com2name], NA)
+  
+  output = outputDF %>%
+    dplyr::select(model, model2, simID, env1Name:com2)
+  
+  return(output)  
 }
