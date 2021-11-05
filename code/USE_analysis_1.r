198617e1389f7a1aa96440c3a25d7b1f95e415e4
Allen Hurlbert <hurlbert@bio.unc.edu>
2021-11-05 09:55:22 -0400

    updating plots with tweak to parameter key

--- code/USE_analysis_1.r
+++ code/USE_analysis_1.r
@@ -73,7 +73,7 @@ modelColors$color = as.character(modelColors$color)
 
 corrOutput2 = left_join(corrOutput, modelColors, by = 'model')
 
-
+write.csv(corrOutput2, "experiments/uniform_sampling_experiment/treeMetric_parameter_correlation_output.csv", row.names = F)
 
 
 # Plotting correlation coefficients by model for individual tree metrics
@@ -152,27 +152,10 @@ for (exp in c('env', 'nic', 'dis', 'mut', 'com')) {
   # legend panel
   plot(1, 1, type = 'n', xlab = '', ylab = '', yaxt = 'n', xaxt = 'n', bty = 'n')
   
-  #numMods = nrow(tmp)
-  #if (numMods <= 6) {
-    points(rep(0.58, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)),
-           pch = 18, col = tmp$color, cex = 2)
-    text(rep(.61, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)), 
-         paste(tmp$model, '-', tmp$parameter), cex = 1.3, adj = 0)
-    
-  #} else {
-  #  firstHalf = round(numMods/2)
-    
-  #  points(rep(0.58, firstHalf), seq(0.62, 1.38, length.out = firstHalf),
-  #         pch = 18, col = tmp$color[1:firstHalf], cex = 2)
-  #  text(rep(.61, firstHalf), seq(0.62, 1.38, length.out = firstHalf), 
-  #       paste(tmp$model[1:firstHalf], '-', tmp$parameter[1:firstHalf]), cex = 1.1, adj = 0)
-    
-  #  points(rep(1.08, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)),
-  #         pch = 18, col = tmp$color[(firstHalf + 1):numMods], cex = 2)
-  #  text(rep(1.12, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)), 
-  #       paste(tmp$model[(firstHalf + 1):numMods], '-', tmp$parameter[(firstHalf + 1):numMods]), cex = 1.1, adj = 0)
-    
-  #}
+  points(rep(0.58, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)),
+         pch = 18, col = tmp$color, cex = 2)
+  text(rep(.61, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)), 
+       paste(tmp$model, '-', tmp$parameter), cex = 1.3, adj = 0)
   
   mtext(paste(experiments$phrase[experiments$experiment == exp], "experiment"), 3, outer = T, cex = 2, line = 1)
   mtext("correlation coefficient", 1, outer = T, cex = 1.5, line = 0, at = .3)
@@ -185,56 +168,6 @@ dev.off()
 
 
 
-# Plotting correlation coefficients by model
-pdf(paste0('figures/USE_corr_plots_', Sys.Date(), '.pdf'), height = 8, width = 10)
-
-for (exp in c('env', 'nic', 'dis', 'mut', 'com')) {
-  
-  layout(matrix(c(1:15, 15), nrow = 4, byrow = T))
-  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), cex.axis = 1.3)
-  
-  for (met in unique(corrOutput2$metric)) {
-    
-    tmp = filter(corrOutput2, experiment == exp, metric == met)
-    
-    plot(tmp$r, 1:nrow(tmp), pch = 18, col = tmp$color, 
-         cex = 1.8, xlim = c(-1, 1), xlab = '', yaxt = 'n', ylab = '', ylim = c(0.5, 1.1*nrow(tmp)))
-    abline(v = 0, col = 'black', lwd = 2)
-    segments(tmp$r.L95, 1:nrow(tmp), tmp$r.U95, 1:nrow(tmp), col = tmp$color, lwd = 2.5)
-    text(-1, nrow(tmp), met, cex = 1.5, adj = c(0, 1))
-    
-  }
-  # legend panel
-  plot(1, 1, type = 'n', xlab = '', ylab = '', yaxt = 'n', xaxt = 'n', bty = 'n')
-  
-  numMods = nrow(tmp)
-  if (numMods <= 6) {
-    points(rep(0.58, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)),
-           pch = 18, col = tmp$color, cex = 2)
-    text(rep(.61, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)), 
-         paste(tmp$model, '-', tmp$parameter), cex = 1.3, adj = 0)
-    
-  } else {
-    firstHalf = round(numMods/2)
-    
-    points(rep(0.58, firstHalf), seq(0.62, 1.38, length.out = firstHalf),
-           pch = 18, col = tmp$color[1:firstHalf], cex = 2)
-    text(rep(.61, firstHalf), seq(0.62, 1.38, length.out = firstHalf), 
-         paste(tmp$model[1:firstHalf], '-', tmp$parameter[1:firstHalf]), cex = 1.1, adj = 0)
-    
-    points(rep(1.08, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)),
-           pch = 18, col = tmp$color[(firstHalf + 1):numMods], cex = 2)
-    text(rep(1.12, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)), 
-         paste(tmp$model[(firstHalf + 1):numMods], '-', tmp$parameter[(firstHalf + 1):numMods]), cex = 1.1, adj = 0)
-    
-  }
-  
-  mtext(paste(experiments$phrase[experiments$experiment == exp], "experiment"), 3, outer = T, cex = 2, line = 1)
-  mtext("correlation coefficient", 1, outer = T, cex = 1.5, line = 0, at = .3)
-  
-  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), mfrow = c(4, 4))
-}
-dev.off()
 
 
 
