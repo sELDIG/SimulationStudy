601185ea264e584d6dce5c891f16789b214bab4e
Allen Hurlbert <hurlbert@bio.unc.edu>
2021-11-04 20:35:33 -0400

    now plotting pca correlations

--- code/USE_analysis_1.r
+++ code/USE_analysis_1.r
@@ -39,6 +39,9 @@ corrOutput = data.frame(model = character(),
                         r.L95 = double(),
                         r.U95 = double())
 
+metriccolumns = processDFmetrics %>%
+  select(S:VPD, nLTT_stat:PC6) %>% names()
+
 for (e in experiments$experiment) {
   
   relevantModels = filter(paramKey, experiment == e) %>% 
@@ -49,7 +52,8 @@ for (e in experiments$experiment) {
     # Check for new model-experiment combinations to run
     if (!paste(mod, e) %in% paste(corrOutput$model, corrOutput$experiment)) {
       
-      corDF = corrCalcUSE(processDFmetrics, e, mod)
+      corDF = corrCalcUSE(processDFmetrics, e, mod, 
+                          metrics = metriccolumns)
       
       if (class(corDF) == 'data.frame') {
         corrOutput = rbind(corrOutput, corDF)
@@ -68,6 +72,115 @@ modelColors$model = as.character(modelColors$model)
 modelColors$color = as.character(modelColors$color)
 
 corrOutput2 = left_join(corrOutput, modelColors, by = 'model')
+
+
+
+
+# Plotting correlation coefficients by model for individual tree metrics
+pdf(paste0('figures/USE_corr_plots_', Sys.Date(), '.pdf'), height = 8, width = 10)
+
+for (exp in c('env', 'nic', 'dis', 'mut', 'com')) {
+  
+  layout(matrix(c(1:15, 15), nrow = 4, byrow = T))
+  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), cex.axis = 1.3)
+  
+  for (met in c('log10S', 'PD', 'Gamma', 'Beta', 'Colless', 'Sackin', 'Yule.PDA.ratio', 
+                'MRD', 'VRD', 'PSV', 'mean.Iprime', 'MPD', 'VPD', 'nLTT_stat')) {
+    
+    tmp = filter(corrOutput2, experiment == exp, metric == met)
+    
+    plot(tmp$r, 1:nrow(tmp), pch = 18, col = tmp$color, 
+         cex = 1.8, xlim = c(-1, 1), xlab = '', yaxt = 'n', ylab = '', ylim = c(0.5, 1.1*nrow(tmp)))
+    abline(v = 0, col = 'black', lwd = 2)
+    segments(tmp$r.L95, 1:nrow(tmp), tmp$r.U95, 1:nrow(tmp), col = tmp$color, lwd = 2.5)
+    text(-1, nrow(tmp), met, cex = 1.5, adj = c(0, 1))
+    
+  }
+  # legend panel
+  plot(1, 1, type = 'n', xlab = '', ylab = '', yaxt = 'n', xaxt = 'n', bty = 'n')
+  
+  numMods = nrow(tmp)
+  if (numMods <= 6) {
+    points(rep(0.58, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)),
+           pch = 18, col = tmp$color, cex = 2)
+    text(rep(.61, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)), 
+         paste(tmp$model, '-', tmp$parameter), cex = 1.3, adj = 0)
+    
+  } else {
+    firstHalf = round(numMods/2)
+    
+    points(rep(0.58, firstHalf), seq(0.62, 1.38, length.out = firstHalf),
+           pch = 18, col = tmp$color[1:firstHalf], cex = 2)
+    text(rep(.61, firstHalf), seq(0.62, 1.38, length.out = firstHalf), 
+         paste(tmp$model[1:firstHalf], '-', tmp$parameter[1:firstHalf]), cex = 1.1, adj = 0)
+    
+    points(rep(1.08, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)),
+           pch = 18, col = tmp$color[(firstHalf + 1):numMods], cex = 2)
+    text(rep(1.12, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)), 
+         paste(tmp$model[(firstHalf + 1):numMods], '-', tmp$parameter[(firstHalf + 1):numMods]), cex = 1.1, adj = 0)
+    
+  }
+  
+  mtext(paste(experiments$phrase[experiments$experiment == exp], "experiment"), 3, outer = T, cex = 2, line = 1)
+  mtext("correlation coefficient", 1, outer = T, cex = 1.5, line = 0, at = .3)
+  
+  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), mfrow = c(4, 4))
+}
+dev.off()
+
+
+
+# Plotting correlation coefficients by model for tree metric PRINCIPAL COMPONENTS
+pdf(paste0('figures/USE_corr_plots_PCs_', Sys.Date(), '.pdf'), height = 6, width = 8)
+
+for (exp in c('env', 'nic', 'dis', 'mut', 'com')) {
+  
+  layout(matrix(c(1, 2, 3, 4, 5, 5), nrow = 2, byrow = F))
+  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), cex.axis = 1.3)
+  
+  for (met in c('PC1', 'PC2', 'PC3', 'PC4')) {
+    
+    tmp = filter(corrOutput2, experiment == exp, metric == met)
+    
+    plot(tmp$r, 1:nrow(tmp), pch = 18, col = tmp$color, 
+         cex = 1.8, xlim = c(-1, 1), xlab = '', yaxt = 'n', ylab = '', ylim = c(0.5, 1.1*nrow(tmp)))
+    abline(v = 0, col = 'black', lwd = 2)
+    segments(tmp$r.L95, 1:nrow(tmp), tmp$r.U95, 1:nrow(tmp), col = tmp$color, lwd = 2.5)
+    text(-1, nrow(tmp), met, cex = 1.5, adj = c(0, 1))
+    
+  }
+  # legend panel
+  plot(1, 1, type = 'n', xlab = '', ylab = '', yaxt = 'n', xaxt = 'n', bty = 'n')
+  
+  #numMods = nrow(tmp)
+  #if (numMods <= 6) {
+    points(rep(0.58, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)),
+           pch = 18, col = tmp$color, cex = 2)
+    text(rep(.61, nrow(tmp)), seq(0.62, 1.38, length.out = nrow(tmp)), 
+         paste(tmp$model, '-', tmp$parameter), cex = 1.3, adj = 0)
+    
+  #} else {
+  #  firstHalf = round(numMods/2)
+    
+  #  points(rep(0.58, firstHalf), seq(0.62, 1.38, length.out = firstHalf),
+  #         pch = 18, col = tmp$color[1:firstHalf], cex = 2)
+  #  text(rep(.61, firstHalf), seq(0.62, 1.38, length.out = firstHalf), 
+  #       paste(tmp$model[1:firstHalf], '-', tmp$parameter[1:firstHalf]), cex = 1.1, adj = 0)
+    
+  #  points(rep(1.08, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)),
+  #         pch = 18, col = tmp$color[(firstHalf + 1):numMods], cex = 2)
+  #  text(rep(1.12, (numMods - firstHalf)), seq(0.62, 1.38, length.out = (numMods - firstHalf)), 
+  #       paste(tmp$model[(firstHalf + 1):numMods], '-', tmp$parameter[(firstHalf + 1):numMods]), cex = 1.1, adj = 0)
+    
+  #}
+  
+  mtext(paste(experiments$phrase[experiments$experiment == exp], "experiment"), 3, outer = T, cex = 2, line = 1)
+  mtext("correlation coefficient", 1, outer = T, cex = 1.5, line = 0, at = .3)
+  
+  par(mar = c(4, 4, 0, 1), oma = c(3, 0, 4, 0), mgp = c(2.2, 1, 0), mfrow = c(4, 4))
+}
+dev.off()
+
 
 
 
@@ -126,6 +239,7 @@ dev.off()
 
 
 
+
 # Plot of distribution of correlation coefficients by model
 nModels = length(unique(corrOutput2$model))
 
@@ -158,6 +272,7 @@ for (exp in c('env', 'nic', 'dis', 'mut', 'com')) {
   hist(corrOutput2$r[corrOutput2$experiment == exp], xlab = '', main = '', col = 'gray50', 
        xlim = c(-1, 1), breaks = seq(-1, 1, by = .1))
   legend("topleft", exp, bty = 'n', cex = 2)
+  abline(v = mean(corrOutput2$r[corrOutput2$experiment == exp], na.rm = T), col = 'red')
 }
 mtext("Correlation coefficient", 1, cex = 2, outer = T, line = 1)
 dev.off()
