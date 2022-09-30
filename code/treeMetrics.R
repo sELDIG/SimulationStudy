1614d5e1bc64cd80d94f900ffebc7f681457894d
Allen Hurlbert <hurlbert@bio.unc.edu>
2022-09-30 14:33:21 -0400

    adding analysis of empirical sister clades

--- code/treeMetrics.R
+++ code/treeMetrics.R
@@ -144,7 +144,7 @@ treeMetrics = function(treeInput) {
 
   # Prune out extinct species
   treeExtant = tryCatch({
-    drop.extinct(treeInput, tol = 1e-8)
+    drop.extinct(treeInput, tol = 1e-7)
   }, error = function(e) {
     treeExtant = drop.fossil(treeInput)
   })
@@ -439,4 +439,172 @@ metricsForManyTrees = function(treefiles = NULL, minimumTreeSize = 20, fileOut,
     
   }  
   
+}
+
+
+
+
+###########################################################################
+# Calculating various attributes on a phylogeny in Newick format using
+# Thijs Janzen's treestats package
+# https://github.com/thijsjanzen/treestats
+#
+#   treeInput: a phylogenetic tree of class phylo
+
+treeStats = function(treeInput) {
+  
+  if(class(treeInput) != "phylo") {
+    stop("The object passed to treeMetrics() is not of class 'phylo'")
+  }
+  
+  require(ape)
+  require(caper)
+  require(geiger)
+  require(picante)
+  require(apTreeshape)
+  require(dispRity)
+  require(RPANDA)
+  require(nLTT)
+  require(stringr)
+  require(Rfast)
+  require(castor)
+  require(treestats)
+  
+  # Drop root edge
+  treeInput$root.edge = 0
+  
+  # Prune out extinct species
+  treeExtant = tryCatch({
+    drop.extinct(treeInput, tol = 1e-8)
+  }, error = function(e) {
+    treeExtant = drop.fossil(treeInput)
+  })
+  
+  # Randomly resolve any existing polytomies
+  tree = multi2di(treeExtant)
+  
+  # Richness
+  S = length(tree$tip.label)
+  log10S = log10(S)
+  
+  treestats = calc_all_stats(tree)
+  
+  # Absolute tree length, which is the diagonal of the vcv matrix
+  # (distance from tip to root; https://bio.libretexts.org/Bookshelves/Evolutionary_Developmental_Biology/Phylogenetic_Comparative_Methods_(Harmon)/03%3A_Introduction_to_Brownian_Motion/3.04%3A_Brownian_Motion_on_a_Phylogenetic_Tree)
+  v.matrix = vcv(tree, corr=F)
+  tree.length = diag(v.matrix)[1]
+  
+  # Tree with branch lengths scaled by total tree length
+  tree.scaled = tree
+  tree.scaled$edge.length = tree$edge.length/tree.length
+  
+  # PD (phylogenetic diversity, summed branch lengths) on scaled tree
+  PD = sum(tree.scaled$edge.length)
+  
+  # Create treeshape objects for certain calculations
+  tree.shape = as.treeshape(tree)
+  tree.shape.scaled = as.treeshape(tree.scaled)
+  
+  #Pybus & Harvey (2000)'s gamma statistic
+  gamma.stat = gammaStat(tree.scaled)
+  
+
+  # Mean Pairwise Distance (in scaled tree)
+  tryCatch({
+    pairwise.dists = dist.nodes(tree.scaled)
+    sum.pairwise.dists = upper_tri(pairwise.dists, diag = FALSE, suma = TRUE)
+    MPD = sum.pairwise.dists/(ncol(pairwise.dists)*(ncol(pairwise.dists)-1)/2)
+  }, error = function(e) {
+    MPD = NA
+  })
+  
+  # Variance Pairwise Distance (in scaled tree)
+  tryCatch({
+    tri.pairwise.dists = upper_tri(pairwise.dists, diag = FALSE, suma = FALSE)
+    VPD = var(as.vector(tri.pairwise.dists))
+  }, error = function(e) {
+    VPD = NA
+  })
+  
+  
+  # Some metrics are difficult to calculate on very large trees.
+  # Also calculate RPANDA spectral density metrics (Lewitus & Morlon 2016)
+  
+  # RPANDA analyses seem to bonk on very large phylogenies, so only try calculating for fewer than 6000 species
+  if(S < 6000) {
+    
+    # RPANDA
+    tryCatch({
+      MGL = spectR(tree)
+      MGL_principal_eigenvalue = MGL$principal_eigenvalue 
+      MGL_asymmetry = MGL$asymmetry  
+      MGL_peakedness = MGL$peakedness
+      MGL_eigengap = MGL$eigengap
+    }, error = function(e) {
+      MGL_principal_eigenvalue = NA
+      MGL_asymmetry = NA
+      MGL_peakedness = NA
+      MGL_eigengap = NA
+    })
+    
+  } else {
+    MGL_principal_eigenvalue = NA
+    MGL_asymmetry = NA  
+    MGL_peakedness = NA
+    MGL_eigengap = NA
+  }
+  
+  # Colless index
+  Colless = colless(tree.shape)
+  
+  # Sackin index
+  Sackin = sackin(tree.shape)
+  
+  # logarithm of the ratio of the likelihoods under the Yule model and the PDA model
+  Yule.PDA.ratio = shape.statistic(tree.shape)
+  
+  # Mean Root Distance (MRD, also abbreviated N-bar, Shao & Sokal 1990) (Agapow & Purvis 2002)
+  # using castor package
+  # --get_all_distances_to_root returns tip and node distances; only want tip distances which are returned first
+  
+  root.dist <- get_all_distances_to_root(tree, as_edge_count = TRUE)[1:(tree$Nnode+1)]
+  MRD = mean(root.dist)
+  
+  # Variance in root distances across the tips, Shao & Sokal 1990 (Agapow & Purvis 2002)
+  VRD = var(root.dist)
+  
+  # Calculate Helmus et al. 2007's Phylogenetic Species Variability (PSV) metric
+  # based on the phylogenetic variance/covariance matrix
+  # While Helmus et al.'s equation is as follows
+  # PSV = (n*trace(C) - sum(C))/(n*(n-1)) 
+  # this only holds for a tree with contemporaneous tips where C is the correlation matrix.
+  # When the tree is not ultra-metric, Algar et al. 2009 (Ecol Lett) show the calculation
+  # using the variance-covariance matrix, V:
+  # PSV = (n*trace(V) - sum(V))/(trace(V)*(n-1)) 
+  
+  n = nrow(v.matrix)
+  PSV = (n*sum(diag(v.matrix)) - sum(v.matrix))/(sum(diag(v.matrix))*(n-1))
+  
+  # mean I' from Purvis et al. 2002. Evaluating phylogenetic tree shape: two modifications to Fusco & Cronk's method
+  if(tree$Nnode >= 3) {
+    tree$node.label = NULL
+    fusco = caper::fusco.test(tree)
+    mean.Iprime = fusco$mean.Iprime
+  } else {
+    mean.Iprime = NA
+  }
+  
+  # nLTT statistic on first example tree
+  utils::data(exampleTrees, package = "nLTT") # Set of birth-death trees
+  nLTT_stat <- nLTT::nLTTstat(
+    tree1 = exampleTrees[[1]], # Can be changed to generated Yule tree or any other tree
+    tree2 = tree
+  )
+  
+  return(list(S = S, log10S = log10S, tree.length = tree.length, PD = PD, Gamma = gamma.stat, Beta = beta.stat, 
+              Colless = Colless, Sackin = Sackin, Yule.PDA.ratio = Yule.PDA.ratio, MRD = MRD, 
+              VRD = VRD, PSV = PSV, mean.Iprime = mean.Iprime,
+              MPD = MPD, VPD = VPD, 
+              MGL_principal_eigenvalue = MGL_principal_eigenvalue, MGL_asymmetry = MGL_asymmetry, 
+              MGL_peakedness = MGL_peakedness, MGL_eigengap = MGL_eigengap, nLTT_stat = nLTT_stat))
 }
--- experiments/uniform_sampling_experiment/SimPaper2_AH.R
+++ experiments/uniform_sampling_experiment/SimPaper2_AH.R
@@ -1,3 +1,13 @@
+# This script 
+# 1) examines simulated trees and metrics and relates them to simulation parameter values.
+# 2) identifies which process-parameters are most correlated with which tree metrics
+# 3) conducts random forest models for describing which metrics are most predictive of parameter values
+# 4) inverts the random forest models to infer estimated parameter values given tree shape metrics for several pairs of sister clades
+
+library(RColorBrewer)
+library(dplyr)
+
+# Read in simulated tree data
 simuData <- read.csv("./experiments/uniform_sampling_experiment/process_parameter_values_and_tree_metrics_sign_corrected.csv", stringsAsFactors = T)
 simuDataKey <- read.csv("./experiments/uniform_sampling_experiment/simulation_parameters_key.csv")
 
@@ -145,7 +155,8 @@ for(i in 1:length(statisticsIndices)){
 }
 
 
-
+########################################################################################
+# Random Forests for predicting parameter values from tree metrics
 
 library(ranger)
 
@@ -190,4 +201,265 @@ for(i in 1:length(statisticsIndices)){
          labels = round(predMean[i,j] / predSD[i,j], digits = 2))
   }
 }
+
+#######################################################################################################
+# Empirical sister clades
+empiricalSisterCladeFiles = list.files('trees/empirical')[grepl('pair', list.files('trees/empirical'))]
+
+# run a version of metricsForManyTrees() to get tree metric output (but that function assumes diff model ID naming convention,
+# so had to manually tweak)
+empiricalMetrics = read.table('empiricalSisterClades_treeOutput.txt', header = T, sep = '\t')
+empiricalMetrics$clade = rep(c(1,2), 6)
+empMetrics = empiricalMetrics[, 4:22]
+
+predictability = array(dim = c(length(statistics), length(predictors), length(models)))
+predictabilityR2 = array(dim = c(length(predictors), length(models)))
+
+
+# Random forests for each model, saving predictions of parameter values from random forests for each empirical tree
+predictedValues = data.frame(tree = character(),
+                             model = character(), 
+                             predictor = character(),
+                             value = numeric())
+
+for(j in 1:length(predictorsIndices)){
+  for(k in 1:length(models)){
+    tmp = simuData[simuData$model == models[k],c(predictorsIndices[j], statisticsIndices)]
+    tmp = tmp[complete.cases(tmp),]
+    if(nrow(tmp) > 0){
+      form = as.formula(paste(predictors[j], "~ ."))
+      x = ranger(form, data = tmp, importance = 'permutation',
+                 scale.permutation.importance = T)
+      predictions = predict(x, data = empMetrics)
+      
+      tmpPredictions = data.frame(tree = word(empiricalMetrics$tree, 1, sep = fixed('.')),
+                                  model = rep(models[k], nrow(empMetrics)),
+                                  predictor = rep(predictors[j], nrow(empMetrics)),
+                                  value = predictions$predictions)
+      
+      predictedValues = rbind(predictedValues, tmpPredictions)
+
+    } 
+  }
+}
+
+
+
+
+# Need to standardize the predicted parameter values relative to the range of values examined within a given model.  
+#   scaled = (x - min) / (max - min)          so first need to identify min and max values for each model-parameter combo:
+
+# (OR DO QUANTILES MAKE MORE SENSE?)
+
+modelParamRange = simuData %>%
+  group_by(model) %>%
+  summarize(env1Min = min(env1, na.rm = T),
+            env1Max = max(env1, na.rm = T),
+            com1Min = min(com1, na.rm = T),
+            com1Max = max(com1, na.rm = T),
+            dis1Min = min(dis1, na.rm = T),
+            dis1Max = max(dis1, na.rm = T),
+            nic1Min = min(nic1, na.rm = T),
+            nic1Max = max(nic1, na.rm = T),
+            mut1Min = min(mut1, na.rm = T),
+            mut1Max = max(mut1, na.rm = T))
+
+# Note that for some model/parameter combinations, the modeler varied parameter values uniformly on a log scale,
+# while for others values varied uniformly on an arithmetic scale.
+
+# I examined the skewness values of all sets of values, and determined that each of the following combinations
+# had a skewness of 0.7 or greater and merited being log-transformed prior to scaling. 
+# (Otherwise, the scaled values, e.g. in model 'hs' for dis1 or mut1 are close to 0)
+
+# ca: mut1, com1
+# hs: dis1, mut1
+# xe: dis1, mut1
+
+modelParamRange$com1Min[modelParamRange$model == 'ca'] = log10(modelParamRange$com1Min[modelParamRange$model == 'ca'])
+modelParamRange$com1Max[modelParamRange$model == 'ca'] = log10(modelParamRange$com1Max[modelParamRange$model == 'ca'])
+modelParamRange$dis1Min[modelParamRange$model %in% c('hs', 'xe')] = log10(modelParamRange$dis1Min[modelParamRange$model %in% c('hs', 'xe')])
+modelParamRange$dis1Max[modelParamRange$model %in% c('hs', 'xe')] = log10(modelParamRange$dis1Max[modelParamRange$model %in% c('hs', 'xe')])
+modelParamRange$mut1Min[modelParamRange$model %in% c('hs', 'xe', 'ca')] = log10(modelParamRange$mut1Min[modelParamRange$model %in% c('hs', 'xe', 'ca')])
+modelParamRange$mut1Max[modelParamRange$model %in% c('hs', 'xe', 'ca')] = log10(modelParamRange$mut1Max[modelParamRange$model %in% c('hs', 'xe', 'ca')])
+
+# Also need to log-transform these in predictedValues
+
+predictedValues$value[predictedValues$model == 'ca' & predictedValues$predictor %in% c('mut1', 'com1')] = 
+  log10(predictedValues$value[predictedValues$model == 'ca' & predictedValues$predictor %in% c('mut1', 'com1')])
+
+predictedValues$value[predictedValues$model %in% c('hs', 'xe') & predictedValues$predictor %in% c('mut1', 'dis1')] = 
+  log10(predictedValues$value[predictedValues$model %in% c('hs', 'xe') & predictedValues$predictor %in% c('mut1', 'dis1')])
+
+
+
+# Rearrange
+modelParamMinMax = data.frame(model = rep(modelParamRange$model, 5), 
+                              predictor = rep(c('env1', 'com1', 'dis1', 'nic1', 'mut1'), each = 8),
+                              minVal = c(modelParamRange$env1Min, modelParamRange$com1Min, modelParamRange$dis1Min, 
+                                         modelParamRange$nic1Min, modelParamRange$mut1Min),
+                              maxVal = c(modelParamRange$env1Max, modelParamRange$com1Max, modelParamRange$dis1Max, 
+                                         modelParamRange$nic1Max, modelParamRange$mut1Max))
+
+
+
+# Function for rescaling a value between the min and max such that the scaled value varies between 0 and 1.
+# If either the min or max are NA, Inf, or -Inf, returns NA
+scaleFunction = function(value, min, max) {
+  
+  if (!min %in% c(-Inf, NA, Inf) & !max %in% c(-Inf, NA, Inf)) {
+    
+    scaledValue = (value - min) / (max - min)
+  
+  } else {
+  
+    scaledValue = NA
+  
+  }
+  return(scaledValue)
+}
+
+
+
+modelColors = data.frame(model = unique(predictedValues$model),
+                         color = as.character(colorSelection(length(unique(predictedValues$model)))))
+
+
+scaledPredictions = predictedValues %>%
+  left_join(modelParamMinMax, by = c('model', 'predictor')) %>%
+  mutate(pair = str_extract(tree, "[1-9]"),
+         clade = rep(c(1, 2), 174),
+         scaledValue = apply(scaledPredictions[, c('value', 'minVal', 'maxVal')], 1, 
+                             function(x) scaleFunction(value = x[1], min = x[2], max = x[3]))) %>%
+  left_join(modelColors, by = 'model')
+
+
+
+
+# Then can plot distribution of scaled scores between sister clades (connecting points within models) for all processes
+pdf('figures/empirical_sisterclade_comparisons.pdf', height = 8, width = 10)
+
+pairs = unique(scaledPredictions$pair)
+processes = unique(scaledPredictions$predictor)
+
+for (pro in processes) {
+  
+  for (p in pairs) {
+    
+    tmp = filter(scaledPredictions, pair == p, predictor == pro)
+    
+    if (p == 1) {
+      
+      par(mfrow = c(2, 3), mar = c(3, 3, 3, 0), oma = c(0, 3, 3, 0))
+
+    } 
+    plot(c(0.5,2.5), range(tmp$scaledValue), type = 'n', xaxt = 'n', xlab = "", ylab = "", las = 1,
+         main = word(tmp$tree[2], 2, sep = "_"))
+    axis(1, at = 1:2, labels = c('clade 1', 'clade 2'))
+    
+    for (m in unique(tmp$model)) {
+      
+      points(tmp$clade[tmp$model==m], tmp$scaledValue[tmp$model==m], type = 'b', col = tmp$color[tmp$model==m], lwd = 3)
+      
+    }
+  }
+
+  mtext(pro, side = 3, outer = TRUE, cex = 2)
+  mtext("Scaled parameter", 2, outer = TRUE, cex = 1.5)
+}
+dev.off()
+
+
+########################################################################################
+# A few example figures of a select clade and process, along with the tree metrics 
+# most associated with that process based on earlier correlation analysis
+
+
+# Need to pull out min and max values of each tree metric across simulations for scaling purposes
+processDFmetrics = read.csv('experiments/uniform_sampling_experiment/process_parameter_values_and_tree_metrics.csv')
+
+metricsMinMax = data.frame(metric = names(processDFmetrics)[25:43],
+                           minVal = apply(processDFmetrics[, 25:43], 2, function(x) min(x, na.rm = T)),
+                           maxVal = apply(processDFmetrics[, 25:43], 2, function(x) max(x, na.rm = T)))
+
+
+modelParamRange = simuData %>%
+  group_by(model) %>%
+  summarize(env1Min = min(env1, na.rm = T),
+            env1Max = max(env1, na.rm = T),
+            com1Min = min(com1, na.rm = T),
+            com1Max = max(com1, na.rm = T),
+            dis1Min = min(dis1, na.rm = T),
+            dis1Max = max(dis1, na.rm = T),
+            nic1Min = min(nic1, na.rm = T),
+            nic1Max = max(nic1, na.rm = T),
+            mut1Min = min(mut1, na.rm = T),
+            mut1Max = max(mut1, na.rm = T))
+
+
+
+
+
+
+# Cetartiodactyla and dispersal
+clade1 = read.tree('trees/empirical/pair2_cetartiodactyla sister clade carnivora perrisodactyla.txt')
+clade2 = read.tree('trees/empirical/pair2_cetartiodactyla.txt')
+
+pdf('figures/cetartiodactyla_dispersal.pdf', height = 8, width = 10)
+par(mfcol = c(2, 2), mar = c(3, 4, 1, 1), oma = c(1, 0, 3, 0))
+plot(clade1, show.tip.label = F, main = "Carnivora/Perrisodactyla", cex.main = 1.5)
+plot(clade2, show.tip.label = F, main = "Cetartiodactyla", cex.main = 1.5)
+
+# Metrics associated with dispersal are VRD, mean.Iprime, (Colless, Sackin, Yule, MRD, MGL_principal_eigenvalue)
+
+dispMetrics = c('VRD', 'mean.Iprime', 'Colless', 'Sackin', 'Yule.PDA.ratio', 'MRD', 'MGL_principal_eigenvalue')
+
+
+# Get scaled values of tree metrics relative to the distribution of metrics from all simulated trees
+# In this case it seemed easier/more straightforward to calculate quantiles
+# (in part because the distribution for many of these simulated metrics is highly skewed)
+scaledMetrics = data.frame(metric = dispMetrics, 
+                           clade1 = rep(NA, length(dispMetrics)), 
+                           clade2 = rep(NA, length(dispMetrics)),
+                           metricColor = brewer.pal(length(dispMetrics), 'Blues'))
+
+for (d in dispMetrics) {
+  
+  scaledMetrics$clade1[scaledMetrics$metric == d] = sum(empiricalMetrics[empiricalMetrics$pair== 2 & empiricalMetrics$clade == 1, d] > processDFmetrics[, d], na.rm = T)/nrow(processDFmetrics)
+    
+  scaledMetrics$clade2[scaledMetrics$metric == d] = sum(empiricalMetrics[empiricalMetrics$pair== 2 & empiricalMetrics$clade == 2, d] > processDFmetrics[, d], na.rm = T)/nrow(processDFmetrics)
+
+}
+
+
+
+plot(c(0.5,2.5), range(c(scaledMetrics$clade1, scaledMetrics$clade2), na.rm = T), type = 'n', xaxt = 'n', xlab = "", ylab = "Scaled tree metric", las = 1, cex.lab = 1.3)
+axis(1, at = 1:2, labels = c('Carnivora/\nPerrisodactyla', 'Cetartiodactyla'), tck = -.01)
+
+for (m in scaledMetrics$metric) {
+  
+  points(x = c(1, 2), y = scaledMetrics[scaledMetrics$metric == m, 2:3], 
+         type = 'b', col = scaledMetrics$metricColor[scaledMetrics$metric == m], lwd = 3)
+  
+  text(2.1, scaledMetrics$clade2[scaledMetrics$metric == m], m, adj = 0)
+  
+}
+text(.65, max(scaledMetrics$clade1, na.rm = T), "Metrics", cex = 1.5)
+
+# Inferred dispersal parameters for different models
+dispersalParams = filter(scaledPredictions, pair == 2, predictor == 'dis1')
+
+plot(c(0.5,2.5), range(dispersalParams$scaledValue), type = 'n', xaxt = 'n', xlab = "", ylab = "low <--    Inferred dispersal    --> high", las = 1, cex.lab = 1.3)
+axis(1, at = 1:2, labels = c('Carnivora/\nPerrisodactyla', 'Cetartiodactyla'), tck = -.01)
+
+for (m in unique(dispersalParams$model)) {
+  
+  points(dispersalParams$clade[dispersalParams$model==m], dispersalParams$scaledValue[dispersalParams$model==m], 
+         type = 'b', col = dispersalParams$color[dispersalParams$model==m], lwd = 3)
+  
+  text(2.1, dispersalParams$scaledValue[dispersalParams$model==m & dispersalParams$clade == 2], m, adj = 0)
+  
+}
+text(.65, max(dispersalParams$scaledValue, na.rm = T), "Models", cex = 1.5)
+
+dev.off()
 
